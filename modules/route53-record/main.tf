29d36e13b3aecccac60b5b02fd9ef5b445aeddcf
Scott Cowan <scott.cowan@marketing.delivery>
2024-11-13 16:03:32 +0000

    fix

--- modules/route53-record/main.tf
+++ modules/route53-record/main.tf
@@ -1,11 +1,16 @@
 resource "aws_route53_record" "alias" {
-  zone_id = var.route53_zone_id
-  name    = var.domain_name
-  type    = "A"
+  zone_id         = var.route53_zone_id
+  name            = var.domain_name
+  type            = "A"
+  allow_overwrite = true
 
   alias {
     name                   = var.alb_dns_name
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
+  }
+
+  lifecycle {
+    ignore_changes = [name]
   }
 }
