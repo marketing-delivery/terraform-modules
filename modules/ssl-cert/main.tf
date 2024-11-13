57e9b02f2e1c9b44151af1b562ee62bd3c8b1df9
Scott Cowan <scott.cowan@marketing.delivery>
2024-11-13 16:26:22 +0000

    fix

--- modules/ssl-cert/main.tf
+++ modules/ssl-cert/main.tf
@@ -11,17 +11,13 @@ resource "aws_acm_certificate" "this" {
 # Add the missing Route53 record resource for certificate validation
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => dvo
   }
 
-  name    = each.value.name
-  type    = each.value.type
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
   zone_id = var.route53_zone_id
-  records = [each.value.record]
+  records = [each.value.resource_record_value]
   ttl     = 60
 }
 
