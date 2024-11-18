bd7d7cf5be63e2767ff0d346c36a579eca2c8258
Scott Cowan <scott.cowan@marketing.delivery>
2024-11-18 14:55:41 +0000

    fixing outputs

--- modules/ssl-cert/outputs.tf
+++ modules/ssl-cert/outputs.tf
@@ -5,15 +5,15 @@ output "certificate_arn" {
 
 output "validation_record_name" {
   description = "The name of the DNS record for certificate validation"
-  value       = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  value       = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_name
 }
 
 output "validation_record_value" {
   description = "The value of the DNS record for certificate validation"
-  value       = aws_acm_certificate.example.domain_validation_options[0].resource_record_value
+  value       = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_value
 }
 
 output "validation_record_type" {
   description = "The type of DNS record for certificate validation"
-  value       = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  value       = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_type
 }
