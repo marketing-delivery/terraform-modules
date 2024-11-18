ea108179ac8c157f1389744a547d926b7eab8899
Scott Cowan <scott.cowan@marketing.delivery>
2024-11-18 18:55:35 +0000

    adding task execution role

--- modules/ecs-cluster/outputs.tf
+++ modules/ecs-cluster/outputs.tf
@@ -6,6 +6,6 @@ output "cluster_name" {
   value = aws_ecs_cluster.this.name
 }
 
-# output "task_execution_role_arn" {
-#   value = aws_iam_role.task_execution_role.arn
-# }
+output "task_execution_role_arn" {
+  value = aws_iam_role.task_execution_role.arn
+}
