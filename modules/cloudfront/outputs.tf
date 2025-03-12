b3a351bd1e3a8766394bdd0d8c1f9865f32e8f48
Scott Cowan <scott.cowan@marketing.delivery>
2025-03-12 10:28:18 +0000

    fixing cf reference

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -11,9 +11,9 @@ output "url" {
 }
 
 output "domain_name" {
-  value = aws_cloudfront_distribution.example.domain_name
+  value = aws_cloudfront_distribution.this.domain_name
 }
 
 output "zone_id" {
-  value = aws_cloudfront_distribution.example.hosted_zone_id
+  value = aws_cloudfront_distribution.this.hosted_zone_id
 }
