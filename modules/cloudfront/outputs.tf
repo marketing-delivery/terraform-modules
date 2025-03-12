00cdc4d2fcb10966401df83a0dedd6b48ca50abf
Scott Cowan <scott.cowan@marketing.delivery>
2025-03-12 10:23:33 +0000

    adding outputs for setting up route53

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -9,3 +9,11 @@ output "id" {
 output "url" {
   value = aws_cloudfront_distribution.this.domain_name
 }
+
+output "domain_name" {
+  value = aws_cloudfront_distribution.example.domain_name
+}
+
+output "zone_id" {
+  value = aws_cloudfront_distribution.example.hosted_zone_id
+}
